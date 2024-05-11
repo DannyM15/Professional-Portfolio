4f88fb27e1fb68de7662cb4c9414c0b63191d208
DannyM15 <merinodanny15@gmail.com>
2024-05-11 14:29:30 -0700

    structure made

---
+++ src/components/projects.jsx
@@ -0,0 +1,23 @@
+const name = 'Search Engine Optimization'
+
+
+
+function Projects() {
+    return (<div className="main-container">
+        <div className="container box">
+        <h2 id="Projects" className="box-title">Projects</h2>
+            <ul>Here you can find a few projects to show my range of abilites and attention to detail.</ul>
+
+                <div className="row card">
+                    <h3>{name}</h3>
+                    <p>Here I was tasked with increasing the accecibilty of the website and increasing the visibility by
+                        search engines.</p>
+                    <a href="https://dannym15.github.io/YYH/#search-engine-optimization"></a>
+                </div>
+
+        </div>
+    </div>
+    )
+}
+
+export default Projects;
