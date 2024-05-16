7ff6f39dea6214194a51ca86d572eec46cf067ac
DannyM15 <merinodanny15@gmail.com>
2024-05-15 21:17:51 -0700

    big update

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -13,13 +13,18 @@ function Projects(props) {
     return (
     <div className="card">
         <div className="card-body">
-        {props.project.key}
-        {props.project.title}
+        {props.project.key} 
+      <h3>  {props.project.title} <br /> </h3>
+     <img src={props.project.image}></img> <br /> 
+        {props.project.description}<br /> 
+        {props.project.link}<br /> 
+        {props.project.github}
         </div>
     </div>
     )
 }
-  
+
+
 Projects.propTypes = {
     key: PropTypes.number,
     project: PropTypes.array
